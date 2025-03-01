ed649b0d69d63a83eb86e9fda147845f912d150c
Brenno Henrique Santos Oliveira <brennohh22@hotmail.com>
2025-02-28 21:56:25 -0300

    [Duo e Think] ADD Tela de briefing na semaforização e finalizar checkin na semaforização

--- src/app/jobs/job-tab-status.model.ts
+++ src/app/jobs/job-tab-status.model.ts
@@ -3,6 +3,7 @@ import { EJobTabStatus } from './job-tab-status.enum';
 export class JobTabStatus {
     constructor(
         public info_check?: EJobTabStatus,
+        public briefing_check?: EJobTabStatus,
         public project_check?: EJobTabStatus,
         public descriptive_memorial_check?: EJobTabStatus,
         public checkin_check?: EJobTabStatus,

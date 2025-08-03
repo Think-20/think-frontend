2486ef7e3ec3121fbcc698ac8e0ae98410e22ed0
Brenno Henrique Santos Oliveira <brennohh22@hotmail.com>
2025-08-03 14:49:05 -0300

    Update check-in-other-cnpj.component.ts

--- src/app/check-in/components/check-in-other-cnpj/check-in-other-cnpj.component.ts
+++ src/app/check-in/components/check-in-other-cnpj/check-in-other-cnpj.component.ts
@@ -1,22 +1,74 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { CheckInService } from "app/check-in/check-in.service";
+import { IOtherCnpj } from "app/check-in/models/other-cnpj.model";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
-  selector: 'cb-check-in-other-cnpj',
-  templateUrl: './check-in-other-cnpj.component.html',
-  styleUrls: ['./check-in-other-cnpj.component.scss']
+  selector: "cb-check-in-other-cnpj",
+  templateUrl: "./check-in-other-cnpj.component.html",
+  styleUrls: ["./check-in-other-cnpj.component.scss"],
 })
-export class CheckInOtherCnpjComponent {
-  @ViewChild('inputName', { static: false }) name: ElementRef<HTMLInputElement>;
+export class CheckInOtherCnpjComponent implements AfterViewInit, OnDestroy {
+  @ViewChild("inputName", { static: false }) name: ElementRef<HTMLInputElement>;
 
   @Input() form: FormGroup;
+  @Input() checkInId: number;
   @Output() remove = new EventEmitter<boolean>();
+
+  private onDestroy$ = new Subject<void>();
+
+  constructor(private checkInService: CheckInService) {}
+
+  ngAfterViewInit(): void {
+    this.form.valueChanges
+      .debounceTime(500)
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe(() => {
+        if (this.form.valid) {
+          this.save();
+        }
+      });
+  }
 
   focusName(): void {
     this.name.nativeElement.focus();
   }
 
+  private save(): void {
+    const obj = this.form.value;
+
+    delete obj.identifier;
+
+    obj.checkin_id = this.checkInId;
+
+    (obj.id
+      ? this.checkInService.putOtherCnpj(obj)
+      : this.checkInService.postOtherCnpj(obj)
+    ).subscribe({
+      next: (response) => {
+        this.form.get("id").setValue(response.object.id);
+      },
+      error: (err) => console.error("Error saving CNPJ:", err),
+    });
+  }
+
   delete(): void {
     this.remove.emit(true);
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 }
